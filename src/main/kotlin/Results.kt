3ca4deec6c414db81fba33b7068e9f69cad4b33b
rarnu <hexj@isyscore.com>
2024-01-05 20:57:03 +0800

    fix: Result

--- src/main/kotlin/Results.kt
+++ src/main/kotlin/Results.kt
@@ -2,7 +2,7 @@
 
 package com.isyscore.kotlin.ktor
 
-data class Result<T>(val code: Int, val message: String, val data: T?) {
+data class Result<T>(var code: Int, var message: String, var data: T?) {
     companion object {
         fun successNoData(code: Int = 0, message: String = ""): Result<*> = Result(code, message, null)
         fun <T> success(code: Int = 0, message: String = "", data: T? = null): Result<T> = Result(code, message, data)
@@ -12,13 +12,13 @@ data class Result<T>(val code: Int, val message: String, val data: T?) {
     }
 }
 
-data class PagedData<T>(val current: Int = 1, val size: Int = 20, val total: Int = 0, val pages: Int = 1, val records: List<T>? = null) {
+data class PagedData<T>(var current: Int = 1, var size: Int = 20, var total: Int = 0, var pages: Int = 1, var records: List<T>? = null) {
     companion object {
         fun noData(): PagedData<*> = PagedData<Unit>(records = null)
     }
 }
 
-data class PagedResult<T>(val code: Int, val message: String, val data: PagedData<T>) {
+data class PagedResult<T>(var code: Int, var message: String, var data: PagedData<T>) {
     companion object {
         fun successNoData(code: Int = 0, message: String = ""): PagedResult<*> = PagedResult(code, message, PagedData.noData())
         fun <T> success(code: Int = 0, message: String = "", data: PagedData<T>): PagedResult<T> = PagedResult(code, message, data)
