706926bfccfd831e7a41099e72b09e1a399e0d7f
rarnu <hexj@isyscore.com>
2024-12-27 12:46:21 +0800

    add: Result内的message修改为可空

--- src/main/kotlin/Results.kt
+++ src/main/kotlin/Results.kt
@@ -2,16 +2,16 @@
 
 package com.isyscore.kotlin.ktor
 
-data class Result<T>(var code: Int, var message: String, var data: T?) {
+data class Result<T>(var code: Int, var message: String?, var data: T?) {
     companion object {
         @JvmStatic
-        fun successNoData(code: Int = 0, message: String = ""): Result<*> = Result(code, message, null)
+        fun successNoData(code: Int = 0, message: String? = ""): Result<*> = Result(code, message, null)
         @JvmStatic
-        fun <T> success(code: Int = 0, message: String = "", data: T? = null): Result<T> = Result(code, message, data)
+        fun <T> success(code: Int = 0, message: String? = "", data: T? = null): Result<T> = Result(code, message, data)
         @JvmStatic
-        fun errorNoData(code: Int = 500, message: String = ""): Result<*> = Result(code, message, null)
+        fun errorNoData(code: Int = 500, message: String? = ""): Result<*> = Result(code, message, null)
         @JvmStatic
-        fun <T> error(code: Int = 500, message: String = "", data: T?): Result<T> = Result(code, message, data)
+        fun <T> error(code: Int = 500, message: String? = "", data: T?): Result<T> = Result(code, message, data)
     }
 }
 
@@ -22,15 +22,15 @@ data class PagedData<T>(var current: Int = 1, var size: Int = 20, var total: Int
     }
 }
 
-data class PagedResult<T>(var code: Int, var message: String, var data: PagedData<T>) {
+data class PagedResult<T>(var code: Int, var message: String?, var data: PagedData<T>) {
     companion object {
         @JvmStatic
-        fun successNoData(code: Int = 0, message: String = ""): PagedResult<*> = PagedResult(code, message, PagedData.noData())
+        fun successNoData(code: Int = 0, message: String? = ""): PagedResult<*> = PagedResult(code, message, PagedData.noData())
         @JvmStatic
-        fun <T> success(code: Int = 0, message: String = "", data: PagedData<T>): PagedResult<T> = PagedResult(code, message, data)
+        fun <T> success(code: Int = 0, message: String? = "", data: PagedData<T>): PagedResult<T> = PagedResult(code, message, data)
         @JvmStatic
-        fun errorNoData(code: Int = 500, message: String = ""): PagedResult<*> = PagedResult(code, message, PagedData.noData())
+        fun errorNoData(code: Int = 500, message: String? = ""): PagedResult<*> = PagedResult(code, message, PagedData.noData())
         @JvmStatic
-        fun <T> error(code: Int = 500, message: String = "", data: PagedData<T>): PagedResult<T> = PagedResult(code, message, data)
+        fun <T> error(code: Int = 500, message: String? = "", data: PagedData<T>): PagedResult<T> = PagedResult(code, message, data)
     }
 }
